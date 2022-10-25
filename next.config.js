ec4a18a0ea6f581b96a397a46ad917ce30180a2f
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-26 00:05:12 +0530

    workingisr

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  staticPageGenerationTimeout: 500,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -128,7 +128,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths: paths,
-    fallback: false,
+    fallback: "blocking",
   };
 };
 
