8c4289eca2b4c5b70a723e2574b86ca9f7b53fee
Rahul Sharma <rahulsharma02121998@gmail.com>
2022-10-25 14:45:10 +0530

    implemeted getStaticprops

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,21 @@ import Image from "next/image";
 import Layout from "../components/Layout/Layout";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return <Layout>main</Layout>;
+export default function Home({ countries }) {
+  console.log(countries);
+  return (
+    <Layout>
+      <div className={styles.count}>Found {countries.length} countries</div>
+    </Layout>
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(`${process.env.BASE_URL}/all`);
+  const countries = await res.json();
+  return {
+    props: {
+      countries,
+    },
+  };
+};
